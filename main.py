b67d8cef902fdc69fd68f553f62c982b8c8c83ff
CarlosChato <carlosyalgo20@gmail.com>
2023-10-17 23:02:54 +0200

    continous working on removing background

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from resources.screen import MyApp
 import tkinter as tk
 
-from rembg import remove
-
 if __name__=="__main__":
     # It initializates the App to mainloop it
     ROOT = tk.Tk()
-    ROOT.geometry("700x500")
+    ROOT.geometry("900x700")
     ROOT.configure(background="AntiqueWhite3")
     APP = MyApp(parent=ROOT)
     APP.mainloop()
--- resources/screen.py
+++ resources/screen.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import Entry, Label, messagebox, ttk
-from tkinter.filedialog import askopenfile
+from tkinter.filedialog import askopenfilenames,askopenfile
 from tkinter.constants import SINGLE
+from rembg import remove, new_session
 
 class MyApp(tk.Tk):
     def __init__(self, parent=None):
@@ -9,15 +10,32 @@ class MyApp(tk.Tk):
         tk.Frame.__init__(self, parent)
         self.parent = parent
         self.init_ui()
+        self.files_input = []
 
     def init_ui(self):
         self.parent.title("Background remover")
-        
-        button_test = tk.Button(text="testing", width=20, height=2,
-                                command=lambda:self.select_file())  
-        
-        button_test.grid(pady=25,padx=5)
 
-    def select_file(self):
-        #filename = askopenfile()
-        print(12)
+        label_info = tk.Label(text='selecciona las imágenes que quieras y una vez seleccionadas dele a transformar',
+                              font=18,background="AntiqueWhite3")
+        
+        button_mult_files = tk.Button(text='Open multiple images', width=20, height=2,
+                                command=lambda:self.select_files())
+        
+        label_info.grid(pady=5,padx=50)
+        button_mult_files.grid(pady=40,padx=5)
+
+    def select_files(self):
+        self.files_input = askopenfilenames()
+        print(self.files_input)
+
+    def remover(self):
+        session = new_session()
+
+        for n in self.files_input:
+
+            with open(n.name,'rb') as i:
+                with open(lambda x: x.split('.')[0]+'_out.'+x.split('.')[1](n.name), 'wb') as o:
+                    input = i.read()
+                    out = remove(input,session)
+                    o.write(out)
+        
